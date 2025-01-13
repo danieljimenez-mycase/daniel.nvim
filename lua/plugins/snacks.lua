b5876dd21e6a9bfbc1f99ded8f44e25056a0ac3d
danieljimenez-mycase <daniel.jimenez@mycase.com>
2025-01-13 09:03:12 -0800

    Add snacks lazygit

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    lazygit = { enabled = true },
+  },
+  keys = {
+    {"<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+  }
+}
